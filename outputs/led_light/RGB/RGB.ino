f41d994ee0fe5dcb1094950413dadf000d15574b
Ferrancf5 <ferrancf96@gmail.com>
2020-04-16 21:47:18 +0200

    updated connections and modified Readme of led_light

--- outputs/led_light/RGB/RGB.ino
+++ outputs/led_light/RGB/RGB.ino
@@ -1,6 +1,6 @@
-const int BluePIN = 22; 
-const int GreenPIN = 21; 
-const int RedPIN = 19;
+const int BluePIN = 4; 
+const int GreenPIN = 3; 
+const int RedPIN = 5;
  
 void setup() {
   Serial.begin(9600);    //start serial port
@@ -11,15 +11,19 @@ void setup() {
  
 void loop()
    {    Color(255 ,0 ,0) ;//Red
-        delay(500); 
+        delay(1000); 
         Color(0,255 ,0) ;//Green
-        delay(500);
-        Color(0 ,0 ,255) ;//Blue
-        delay(500);
-        Color(13,227,201); // Mix Color
-        delay(500);
-        Color(0,0,0);
         delay(1000);
+        Color(0 ,0 ,255) ;//Blue
+        delay(1000);
+    //If you have only digital output, you can't send these values.
+    //Because you only send 1 or 0. When you send these values with
+    //digital output, you really send 1 at the same time in all outputs.
+    //For that reason, led will be white
+        Color(13,227,201); // Mix Color or White
+        delay(1500);
+        Color(0,0,0);
+        delay(1500);
    }
 
 void Color(int R, int G, int B)
