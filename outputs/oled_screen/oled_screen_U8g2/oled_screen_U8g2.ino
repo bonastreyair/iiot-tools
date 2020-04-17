6c2af1fa53166690170d3d9fc4d07bc4c0b8a682
bonastreyair <bonastreyair@gmail.com>
2020-04-17 13:58:52 +0200

    changes to oled_screen

--- outputs/oled_screen/oled_screen_U8g2/oled_screen_U8g2.ino
+++ outputs/oled_screen/oled_screen_U8g2/oled_screen_U8g2.ino
@@ -1,11 +1,11 @@
-#include <U8g2lib.h>
-#include <Wire.h>    // Library for I2C communications
+#include <Wire.h>  // Library for I2C communications
+#include <U8g2lib.h>  // Library from U8g2 that enables interaction with the oled screen
 
-#define OLED_SDA       4
-#define OLED_SCL       15
-#define OLED_RST       16
+#define OLED_SDA   4
+#define OLED_SCL   15
+#define OLED_RST   16
 
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, OLED_RST, OLED_SCL, OLED_SDA);   // ESP32 Thing, HW I2C with pin remapping
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C display(U8G2_R0, OLED_RST, OLED_SCL, OLED_SDA);  // ESP32 Thing, HW I2C with pin remapping
 
 int up_time = 0;  // Defines a counter
 int x = 0;
@@ -21,35 +21,29 @@ void setup(){
 
   // Display setup
   display.begin();
-  //display.setFont(u8g2_font_ncenB08_tr); //Sets font and size
+  //display.setFont(u8g2_font_ncenB08_tr);  // Sets font and size
   display.setFont(u8g2_font_unifont_t_symbols);
   
-  //Display Orientation
-  //display.u8g2.setDisplayRotation(U8G2_R0);
+  // Display Orientation
   display.setDisplayRotation(U8G2_R0);
-  //display.u8g2.setDisplayRotation(U8G2_R2);
-  //display.u8g2.setDisplayRotation(U8G2_R3);
-  display.setFlipMode(0);
-  //display.setFlipMode(1);
+  display.setFlipMode(0);  // 0 or 1 depending on the desired flip orientation
 
-
-  //Show some statis text
-  display.clearBuffer();          // clear the internal memory
-  display.drawStr(0, 10, "Hello World!");  // write something to the internal memory
-  display.sendBuffer();          // transfer internal memory to the display
-  delay(2000);
+  // Show some statis text
+  display.clearBuffer();  // Clear the internal memory
+  display.drawStr(0, 10, "Hello World!");  // Write something to the internal memory
+  display.sendBuffer();  // Transfer internal memory to the display
 }
 
 void loop(){
-   for( y = 20; y <= 64; y=y+10){
-    for( x = 0; x <= 128; x=x+10){ 
+   for(y=20; y<=64; y=y+10){
+    for(x=0; x<=128; x=x+10){ 
       display.clearBuffer();
       display.drawGlyph(x, y, 9731);  /* dec 9731/hex 2603 Snowman */
       display.sendBuffer();
-      delay(500);
+      
+      delay(500);  // Freezes the "for" loop for 500 miliseconds 
     }
     x=0;
   }
   y=0;
-
 }
